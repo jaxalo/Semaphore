2fdca951894ac17d881aee475169800c4fc3a2ca
jaxalo <ismail.jattioui1@gmail.com>
2019-05-27 02:44:39 +0200

    more tests

--- launch/main.py
+++ launch/main.py
@@ -6,7 +6,7 @@ from FileProcesser.Writer import Writer
 
 if __name__ == '__main__':
     print("input the name of the directory, it will read all the files.txt that's in it")
-    folder_name = 'launch'
+    folder_name = input()
     program_files_data = Reader.process_folder(folder_name)
     writer = Writer()
     writer.create_reports(program_files_data)
