1622644a028535535f75532af72276e0352f7f85
jaxalo <ismail.jattioui1@gmail.com>
2019-05-27 19:25:15 +0200

    police

--- Instruction/Take.py
+++ Instruction/Take.py
@@ -5,17 +5,6 @@ from Instruction.Instruction import Instruction
 class Take(Instruction):
 
     def execute(self):
-        '''
-        blocked = False
-        if self.semaphore.get_value() <= 0:
-            self.waiting_processes[self.semaphore.get_id()].append(self.process)
-            self.process.block_process()
-            blocked = True
-        else:
-            self.semaphore.decrement()
-        return blocked
-        '''
-
         self.semaphore.decrement()
         if self.semaphore.get_value() < 0:
             self.process.block_process()
